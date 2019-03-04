0f98712a40ba3821d0ac765353b15c860a75774f
chen in company <908294800@qq.com>
2019-03-04 11:38:39 +0800

    知识点整理

--- day10/02 function nest.py
+++ day10/02 function nest.py
@@ -2,13 +2,50 @@
 __author__ = 'brian'
 __date__ = '2019/3/2 0002 22:37'
 
+"""函数嵌套和作用域链"""
+
 """
-    函数嵌套
+函数嵌套调用：
+def func1():
+    pass
+def func2():
+    func1()
+func2()
     
     第一类对象：  ----函数属于此类
         1.可在运行时创建
         2.可作为函数的参数或返回值
         3.可作为变量的实体
+    
+函数可以嵌套定义
+def outer():
+    def inner():
+        pass
+    
+    内部函数可以使用外部函数中的变量，仅使用离他最近的一个外部变量。
+    内部函数定义之后，只能在定义他的直接外部函数中调用
+    nonlocal 关键字 ----仅声明一个离他最近的上层的局部变量，用法及功能类似小global。仅局部变量
+    
+def outer():    # 定义外部函数outer()
+    a = 1   # 声明外部函数中的变量a=1
+    b = 1   # 声明外部函数中的变量b=1
+    def inner():    # 第一层内部函数inner()
+        print('我现在进入了 inner()') # 打印字符串，证明进入了inner()函数
+        print('我在inner()中使用了外部函数outer()中的变量a：'a)    # 打印a的值，这里打印的是outer()中的变量a的值
+        b=3.1414    # 在内部函数inner()中，声明b=3.1414。这里的b不是outer()中的b
+        def inner2():   # 定义第二层内部函数inner2()，他的直接外部函数为inner()，最外层函数为outer()
+            nonlocal b  # 对变量b进行，nonlocal声明。这里的声明会影响到外部函数中的b，
+                        # 如果直接外部函数中没有这个变量。会再在更外层
+            b =3    # 修改b的值
+            print('我现在进入了 inner2')  # 打印字符串，证明进入了inner2函数
+            print('我对变量b进行了nonlocal声明。')
+            print('我改变了b的值，现在b=',b)  # 打印此时inner2中b的值
+        inner2()  # 执行inner2()
+        print('inner中的b为{}，这是因为我再inner2()中声明了nonlocal，且修改了他的值'.format(b)) # 打印inner中的b的值。 这里的b被inner2()中修改了
+                               # 嵌套的函数inner2中由于声明了nonlocal，会影响到这一层
+    inner() # 执行inner()
+    print('outer中的b为{}，这是因为nonlocal没有影响到这一层'.format(b))  # inner2()中的nonlocal声明不会影响到这一层
+outer() # 执行inner()
 """
 
 # 利用函数嵌套，求3个数的最大值
@@ -42,18 +79,40 @@ __date__ = '2019/3/2 0002 22:37'
 #     print('outer中的b',b)  # inner2()中的nonlocal声明不会影响到这一层
 # outer()
 
-def func():
-    print(123)
-func2 = func
-func2()     # 函数名也是一个普通的变量，可以赋值给其他变量
+# def func():
+#     print(123)
+# func2 = func
+# func2()     # 函数名也是一个普通的变量，可以赋值给其他变量
+#
+# l = [func, func2]  # 函数名可以作为容器类型的元素
+# for i in l:
+#     i()
+#
+# def my_func(f):  # 函数名可以参数
+#     f()
+#     return f
+#
+# fun3 = my_func(func)  # 函数名可以作为返回值
+# fun3()
 
-l = [func, func2]  # 函数名可以作为容器类型的元素
-for i in l:
-    i()
-
-def my_func(f):  # 函数名可以参数
-    f()
-    return f
-
-fun3 = my_func(func)  # 函数名可以作为返回值
-fun3()
+# def outer():    # 定义外部函数outer()
+#     a = 1   # 声明外部函数中的变量a=1
+#     b = 1   # 声明外部函数中的变量b=1
+#     def inner():    # 第一层内部函数inner()
+#         print('我现在进入了 inner()') # 打印字符串，证明进入了inner()函数
+#         print('我在inner()中使用了外部函数outer()中的变量a：',a)    # 打印a的值，这里打印的是outer()中的变量a的值
+#         b=3.1414    # 在内部函数inner()中，声明b=3.1414。这里的b不是outer()中的b
+#
+#         def inner2():   # 定义第二层内部函数inner2()，他的直接外部函数为inner()，最外层函数为outer()
+#             nonlocal b  # 对变量b进行，nonlocal声明。这里的声明会影响到外部函数中的b，
+#                         # 如果直接外部函数中没有这个变量。会再在更外层
+#             b =3    # 修改b的值
+#             print('我现在进入了 inner2')  # 打印字符串，证明进入了inner2函数
+#             print('我对变量b进行了nonlocal声明。')
+#             print('我改变了b的值，现在b=',b)  # 打印此时inner2中b的值
+#         inner2()  # 执行inner2()
+#         print('inner中的b为{}，这是因为我再inner2()中声明了nonlocal，且修改了他的值'.format(b)) # 打印inner中的b的值。 这里的b被inner2()中修改了
+#                                # 嵌套的函数inner2中由于声明了nonlocal，会影响到这一层
+#     inner() # 执行inner()
+#     print('outer中的b',b)  # inner2()中的nonlocal声明不会影响到这一层
+# outer() # 执行inner()
--- day10/03 closure.py
+++ day10/03 closure.py
@@ -1,12 +1,24 @@
 """
-    闭包  ----嵌套函数，且内部函数调用外部函数的变量
+    函数名  ----本质上也只是存在内存地址中的一个变量
 
-    闭包的调用  ----可以在全局使用一个内部的函数
+    闭包  ----嵌套函数，且内部函数调用外部函数的变量
+        ：内部函数包含对外部作用域而非全局作用域名字的引用，该内部函数称为闭包函数
+
+    闭包的调用  ----可以在全局使用一个内部的函数，方法是把这个内部函数的函数名返回
+a = 1
+def outer():
+    a = 2
+    def inner():
+        nonlocal a
+        a = 3
+    print(inner.__closure__)  # 如果内部函数的__closure__方法打印了有cell，则该内部函数必为闭包
+    print(a)
+    return inner  # 返回了内部函数名。外部就可用这个内部函数。任何时候都可用
+inn = outer()
+inn()
 
     闭包的好处  ----延长函数内变量等的生存周期
 
-    整理闭包相关知识+day10+默认参数的陷阱  写博客   上午完成
-    下午 晚上  day11 完成  不必整理博客
 """
 # a = 1
 # def outer():
@@ -14,24 +26,22 @@
 #     def inner():
 #         nonlocal a
 #         a = 3
-#
 #     print(inner.__closure__)  # 如果内部函数的__closure__方法打印了有cell，则该内部函数必为闭包
 #     print(a)
 #     return inner  # 返回了内部函数名。外部就可用这个内部函数。任何时候都可用
-# outer()
 #
 # inn = outer()
 # inn()
 
 # import urllib
-from urllib.request import urlopen
-def get_url():
-    url = 'https://www.bilibili.com/'
-    def get_url_inner():
-        ret = urlopen(url).read()
-        ret = ret.decode('utf-8')
-        print(ret)
-    return get_url_inner
-
-get_url_inner = get_url()
-get_url_inner()
+# from urllib.request import urlopen
+# def get_url():
+#     url = 'https://www.bilibili.com/'
+#     def get_url_inner():
+#         ret = urlopen(url).read()
+#         ret = ret.decode('utf-8')
+#         print(ret)
+#     return get_url_inner
+#
+# get_url_inner = get_url()
+# get_url_inner()
