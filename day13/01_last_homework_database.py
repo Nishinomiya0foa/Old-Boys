895cbc86d1464419f2e747fe6828eb7cb5735f46
chen in company <908294800@qq.com>
2019-03-06 17:33:31 +0800

    员工信息表作业

--- day13/01_last_homework_database.py
+++ day13/01_last_homework_database.py
@@ -19,7 +19,134 @@ select 列名1，列名2，… where 列名条件
 select name, age where age>22
 select * where job=IT
 select * where phone like 133
+"""
 
+def do_search_all(split_input):
+    """
+        查询符合条件的所有完整行。
+        split_input: 用户输入的内容，按空格切分之后形成的数组
+        judge: split_input数组的后三个元素
+        cols_menu: employ_ee文件的列名
+        dic_data: employ_ee文件每行的每个元素，按照 列名：值 的键值对存入字典
+        ret_data: 符合查询条件的所有行组成的列表
+        :return 返回ret_data
+    """
+    judge = split_input[-3:]
+    # print(judge)
+    # dic_data = {'id':None, 'name':None, 'age':None, 'phone':None, 'job':None}
+    cols_menu = ['id','name','age','phone','job']
+    dic_data = {}  # 单行记录增城的字典
+    ret_data = []  # 返回的列表
+    with open('employee_data', 'r', encoding='utf-8') as f1:
+        for line in f1:
+            split_data = line.strip('\n').split(',')
+            # print(split_data)
+            for i in cols_menu:
+                dic_data[i] = split_data[cols_menu.index(i)]
+            if judge[0] in dic_data:  #
+                if judge[1] == '>':  # 处理大于号
+                    if int(dic_data[judge[0]]) > int(judge[-1]):  # 大于号前后应该都为数字
+                        ret_data.append(line.strip('\n'))
+                elif judge[1] == '<':
+                    if int(dic_data[judge[0]]) < int(judge[-1]):  #
+                        ret_data.append(line.strip('\n'))
+                elif judge[1] == '=':
+                    if dic_data[judge[0]].lower() == judge[-1]:  # 判断字符串是否相等！
+                        ret_data.append(line.strip('\n'))
+                else:
+                    if judge[-1] in dic_data[judge[0]].lower():  # like 模糊查询
+                        ret_data.append(line.strip('\n'))
+    return ret_data
+
+
+def split_ret_data(f):  # 装饰器函数
+    """select name, age where age>22"""
+    def inner(user_input):
+        ret = f(user_input)  # 执行被装饰的函数，返回整条数据，以列表形式保存
+        user_input_split = user_input.split()  # 用户输入的查询语句被以空格分隔，返回了列表
+        user_see = user_input_split[1:user_input_split.index('where')]  # 用户想要select 的 列名 组成的列表
+        if user_see != ['*']:  # 判断是不是查询显示整条数据
+            cols_menu = ['id', 'name', 'age', 'phone', 'job']  # emplyee_data文件的列名
+            dic_data = {}  # 字典  {查询显示的列名：值}
+            ret_fin = []  # 列表  放查询显示的值
+            for i in ret:
+                temp_lis = i.split(',')  # 去除被装饰函数中的每个， 组成一个新列表
+                for j in range(5):  # 将所有查询到的数据，放入dic_data
+                    dic_data[cols_menu[j]] = temp_lis[j]
+                for i in user_see:  # 将user_see中的列名，找出对应的值。用列表保存
+                    i = i.strip(',')
+                    if i in dic_data:
+                        ret_fin.append(dic_data[i])
+                ret_fin.append('\n')
+            text = ""
+            for i in range(int(len(ret_fin) / len(user_see))):  # 把ret_fin 的数据，更好的显示出来
+                text = ' '.join(ret_fin[i*(len(user_see)+1):(i+1)*(len(user_see)+1)]) + text
+            ret_data = text
+            return ret_data
+        else:  # 查询 * 的情况
+            return '\n'.join(ret)
+    return inner
+
+
+@split_ret_data
+def choose_search_part(s):
+    """
+    查询函数的跳转
+    """
+    s = s.lower()
+    sigh = ['>','<','=','like']
+    split_input = s.split()  # 将输入的按空格切分。放入数组
+    if split_input[0] == 'select' and 'where' in split_input:
+        where_position = split_input.index('where')  # 'where'的索引
+        flag_position = split_input[where_position + 2]  # 处理不够好
+        for sign in sigh:
+            if sign in flag_position:
+                ret_cel = do_search_all(split_input)  # 跳转到查询
+                return ret_cel
+
+
+def add_employee(name='Null',age='Null',phone='Null',job='Null'):
+    """employee_data中新增文件记录"""
+    with open('employee_data', 'r+', encoding='utf-8') as f1:
+        for line in f1:
+            lis = line
+        id = int(line[:lis.find(',')]) +1  # 新增的id应该是上一条的id+1
+        f1.write('\n{id},{name},{age},{phone},{job}'.format(id=id, name=name, age=age, phone=phone,job=job))
+
+
+import os
+def del_employee(user_input_search):
+    """删除某条记录"""
+    user_input = 'select * where ' + user_input_search  # 指引用户输入查询条件
+    employee_list = choose_search_part(user_input)  # 调用 查询的函数。 接收其返回值
+    print('以下为查询结果：')
+    print(employee_list)
+    del_command = input('输入id以删除用户：')  # 输入想删除的记录的id
+    with open('employee_data', 'r+', encoding='utf-8') as f1, open('employee_data.bak', 'w', encoding='utf-8') as f2:
+        for line in f1:
+            if line[:line.index(',')] == del_command:
+                continue
+            f2.write(line)  # 除了想删除的哪行，其余行写入到新文件
+    os.remove('employee_data')  # 删除旧文件
+    os.renames('employee_data.bak','employee_data')  # 新文件改名
+
+
+
+user_input_search = input('select * where ')
+del_employee(user_input_search)
+# ret_cel = choose_search_part('select name, age where age like 2')
+# ret_cel = choose_search_part('select * where phone like 432')
+# print(ret_cel)
+# add_employee('xiaochen', '23','1800000000','worker')
+
+
+
+
+
+
+
+
+"""
 进阶选做：
 b.可创建新员工记录，id要顺序增加
 c.可删除指定员工记录，直接输入员工id即可
