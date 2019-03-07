d914a584b34adb9c201e423a45965240fc521ed2
chen in company <908294800@qq.com>
2019-03-07 16:53:33 +0800

    迭代器与生成器基础

--- day13/01_last_homework_database.py
+++ day13/01_last_homework_database.py
@@ -58,9 +58,10 @@ def do_search_all(split_input):
                         ret_data.append(line.strip('\n'))
     return ret_data
 
-
+from functools import wraps
 def split_ret_data(f):  # 装饰器函数
     """select name, age where age>22"""
+    @wraps(f)
     def inner(user_input):
         ret = f(user_input)  # 执行被装饰的函数，返回整条数据，以列表形式保存
         user_input_split = user_input.split()  # 用户输入的查询语句被以空格分隔，返回了列表
@@ -88,7 +89,7 @@ def split_ret_data(f):  # 装饰器函数
     return inner
 
 
-@split_ret_data
+@split_ret_data  # ->choose_search_part = split_ret_data(choose_search_part)
 def choose_search_part(s):
     """
     查询函数的跳转
@@ -111,7 +112,8 @@ def add_employee(name='Null',age='Null',phone='Null',job='Null'):
         for line in f1:
             lis = line
         id = int(line[:lis.find(',')]) +1  # 新增的id应该是上一条的id+1
-        f1.write('\n{id},{name},{age},{phone},{job}'.format(id=id, name=name, age=age, phone=phone,job=job))
+        f1.write('{id},{name},{age},{phone},{job}'.format(id=id, name=name, age=age, phone=phone,job=job))
+        f1.write('\n')
 
 
 import os
@@ -119,8 +121,12 @@ def del_employee(user_input_search):
     """删除某条记录"""
     user_input = 'select * where ' + user_input_search  # 指引用户输入查询条件
     employee_list = choose_search_part(user_input)  # 调用 查询的函数。 接收其返回值
-    print('以下为查询结果：')
-    print(employee_list)
+    if employee_list:
+        print('以下为查询结果：')
+        print(employee_list)
+    else:
+        print('没有相关记录')
+        return
     del_command = input('输入id以删除用户：')  # 输入想删除的记录的id
     with open('employee_data', 'r+', encoding='utf-8') as f1, open('employee_data.bak', 'w', encoding='utf-8') as f2:
         for line in f1:
@@ -131,15 +137,82 @@ def del_employee(user_input_search):
     os.renames('employee_data.bak','employee_data')  # 新文件改名
 
 
+def alter_employee(user_input_alter:str):
+    """
+        修改文件中某条记录
+        # set name = xxx where id = 3
+    """
+    cols_menu = ['id', 'name', 'age', 'phone', 'job']
+    user_input_alter = user_input_alter.split(' ')
+    user_input_alter_search = ' '.join(user_input_alter[-3:])
+    user_input = 'select * where ' + user_input_alter_search  #
+    employee_record = choose_search_part(user_input)  # 调用 查询的函数。 接收其返回值
+    if employee_record == '':
+        print('没有这条数据')
+    employee_record = employee_record.split('\n')
+    with open('employee_data', 'r', encoding='utf-8') as f1, open('employee_data.bak', 'w', encoding='utf-8') as f2:
+        for line in f1:
+            for i in employee_record:
+                print(i)
+                alter_cont = user_input_alter[1:4]  # 想要修改的内容
+                line_list = line.strip('\n').split(',')
+                if i+'\n' == line:
+                    # 进行修改
+                    for j in cols_menu:
+                        if j == alter_cont[0]:
+                            line_list[cols_menu.index(j)] = alter_cont[2]
+                    line = ','.join(line_list) + '\n'
+            f2.write(line)
+    os.remove('employee_data')
+    os.renames('employee_data.bak', 'employee_data')
+    print('修改完成')
 
-user_input_search = input('select * where ')
-del_employee(user_input_search)
-# ret_cel = choose_search_part('select name, age where age like 2')
-# ret_cel = choose_search_part('select * where phone like 432')
-# print(ret_cel)
-# add_employee('xiaochen', '23','1800000000','worker')
+
+def wrap_login(f):
+    def inner():
+        flag = False
+        while not flag:
+            admin_id = input('管理员账号：')
+            admin_pwd = input('管理员密码：')
+            if admin_id == 'admin' and admin_pwd == '123456':
+                flag = True
+        f()
+    return inner
 
 
+import time
+
+@wrap_login
+def operate():
+    while 1:
+        print('输入查询，新增，删除，修改，q退出')
+        print('选择想要进行的操作：')
+        operation = input('>')
+        if operation.lower() == 'q':
+            print("正在退出系统...")
+            time.sleep(0.5)
+            break
+        elif operation == '查询':
+            search_words = input('输入查询语句：')
+            print(choose_search_part(search_words))
+        elif operation == '新增':
+            add_words_name = input('输入姓名：')
+            add_words_age = input('输入年龄：')
+            add_words_phone = input('输入手机号：')
+            add_words_job = input('输入工作：')
+            add_employee(add_words_name, add_words_age, add_words_phone, add_words_job)
+        elif operation == '删除':
+            print('输入想要删除的记录查询：')
+            del_words = input('select * where ')
+            del_employee(del_words)
+        elif operation == '修改':
+            alter_words = input('输入修改语句：')
+            alter_employee(alter_words)
+        else:
+            print('不能识别您输入的信息。重新输入或退出')
+
+
+operate()
 
 
 
@@ -153,6 +226,7 @@ c.可删除指定员工记录，直接输入员工id即可
 d.修改员工信息
 语法：set 列名=“新的值” where 条件
 #先用where查找对应人的信息，再使用set来修改列名对应的值为“新的值”
+# set name = xxx where id = 3
 
 注意：要想操作员工信息表，必须先登录，登陆认证需要用装饰器完成
 其他需求尽量用函数实现
