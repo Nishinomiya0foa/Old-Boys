b100b6968a27bd63d40709bda425f3a51d9bc60a
chenwenbo <mxstarbucks@sina.com>
2019-03-11 23:40:01 +0800

    面试题

--- interview.py
+++ interview.py
@@ -10,43 +10,50 @@
 
 
 
-def add(n,i):
-    return n+i
-def test():
-    for i in range(4):
-        yield i
-g=test()
-for n in [1,10, 5]:  # n为1或10
-    """由于未调用时，生成器表达式不会执行。拆开来看"""
-    g=(add(n,i) for i in g)  # g = (add(n,i) for i in
-
-# n = 1
-# g = (add(n,i) for i in g)
-# n = 10
-# g = (add(10, i) for i in (add(10,i) for i in (0, 1, 2, 3)))
-#
-# 10 11 12 13
-# 20 21 22 23
+# def add(n,i):
+#     return n+i
+# def test():
+#     for i in range(4):
+#         yield i
+# g=test()
+# for n in [1,10, 5]:  # n为1或10
+#     """由于未调用时，生成器表达式不会执行。拆开来看"""
+#     g=(add(n,i) for i in g)  # g = (add(n,i) for i in
+# print(list(g))
 
 
-# 如果 for n in [1,10,5]
-# n = 1
-# g = (add(n,i) for i in g)
-# n = 10
-# g = (add(10, i) for i in (add(10,i) for i in g))
-# n = 5
-# g = (add(5, i) for i in (add(5,i) for i in (add(n,i) for i in (0, 1, 2, 3))))
-#
-# 5 6 7 8
-# 10 11 12 13
-# 15 16
+# l = list(range(10))
+# n = l.copy
+# print(n)
 
+"""判断字典有没有某个key的方法"""
+# dic = {'b':1}  # 用get可能会有问题
+# if 'x' in dic:
+#     print(True)
 
+"""以下代码输出什么"""
+# def extengList(val, list=[]):
+#     list.append(val)
+#     return list
+# list1 = extengList(10)
+# list2 = extengList(123,[])
+# list3 = extengList('a')
+# print(list1)
+# print(list2)
+# print(list3)
 
+# print(True or False and True)
 
-print(list(g))
+"""
+is 和 == 的区别：
+unicode utf-8 gbk编码之间的关系：
+"""
 
-#    1，0 /1/2/3
-#     1  2  3  4
-
-
+"""算法实现删除list中重复元素"""
+# lis = [1,1,3,3,24,5623,12,31,1,3]
+# lis2 = []
+# for i in lis:
+#     if i not in lis2:
+#         lis2.append(i)
+# lis = lis2
+# print(lis)
