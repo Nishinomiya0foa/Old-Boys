9db3934f6264b8dade389c366f798ff652f20851
chenwenbo <mxstarbucks@sina.com>
2019-05-04 20:32:47 +0800

    练习

---
+++ checkIO/SortArray.py
@@ -0,0 +1,11 @@
+"""按照列表中元素的出现次数进行排序"""
+
+# def frequency_sort(lis:list):
+
+print(max(1,3,34,4))
+
+l = [4,6,2,2,2,6,4,4,4]
+
+for i in l:
+    print(l.count(i))
+
--- test1.py
+++ test1.py
@@ -1,24 +1,9 @@
-"""对 [-2^31, 2^31-1]范围的int类型的数进行反转.
-负号不会被反转
-"""
+num = 52325
+num2 = 12322
+lis = [1,4,2,2,3,1,1,5,4,3,3,3,2]
 
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x < -2**31 or x > 2**31-1:
-            return 0
-        else:
-            list_x = []
-            ret = []
-            [list_x.append(i) for i in str(x)]
-            if list_x[0] == '-':
-                ret.append('-')
-                list_x.remove('-')
-            while len(list_x) > 0:
-                ret.append(list_x.pop())
-            if int(''.join(ret)) < -2 ** 31 or int(''.join(ret)) > 2 ** 31 - 1:
-                return 0
-            return int(''.join(ret))
+def strip_rep(lis:list):
+
 
 if __name__ == '__main__':
-    nnn = Solution()
-    print(nnn.reverse(1534236469))
+    strip_rep(lis)
