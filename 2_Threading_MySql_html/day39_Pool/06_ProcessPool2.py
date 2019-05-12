5dd10df76eb6f98dad0c7a05866ac5efc4f10786
chenwenbo <mxstarbucks@sina.com>
2019-05-12 21:16:25 +0800

    进程池

--- 2_Threading_MySql_html/day39_Pool/06_ProcessPool2.py
+++ 2_Threading_MySql_html/day39_Pool/06_ProcessPool2.py
@@ -12,10 +12,11 @@ def func(n):
     print('end func {}, port: {}'.format(n, os.getpid()))
 
 if __name__ == '__main__':
-    p = Pool(5)
-    for i in range(15):
-        p.apply_async(func, args=(i,))  # p.apply_async()  异步
-                                        # p.apply() 同步
+    # p = Pool(5)
+    with Pool(5) as p:
+        for i in range(15):
+            p.apply_async(func, args=(i,))  # p.apply_async()  异步
+                                            # p.apply() 同步
 
-    p.close()   # 结束进程池接收任务
-    p.join()  # 感知进程池中的任务执行结束
+        p.close()   # 结束进程池接收任务
+        p.join()  # 感知进程池中的任务执行结束
